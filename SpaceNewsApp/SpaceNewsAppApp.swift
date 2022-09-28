c11892b6ba7b212c3568081d7b7a4efe18f47002
Roman Riepa <pwroma@gmail.com>
2022-09-28 12:33:13 +0300

    Initial Commit

---
+++ SpaceNewsApp/SpaceNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsAppApp.swift
+//  SpaceNewsApp
+//
+//  Created by Roman Riepa on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
